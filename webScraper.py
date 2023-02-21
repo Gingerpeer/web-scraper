2229ba65b9d2f5f554d5a07438ef37a8ece2490b
Gingerpeer PC <pierrevanzyl777@gmail.com>
2023-02-21 13:05:05 +0200

    scraping done and accepted without 403 issue

--- webScraper.py
+++ webScraper.py
@@ -1,16 +1,28 @@
 # write a web scraper that will scrape the following website: 
 # This is a web scraping python project where the html is collected from a website. The Html is then used to create an Html document. All links in the main website provided are also scraped and html files created with their page titles as the names
-import requests
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 # Making a GET request
 def scrapeWebsite(url):
+  headers = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+  'Accept-Encoding': 'none',
+  'Accept-Language': 'en-US,en;q=0.8',
+  'Connection': 'keep-alive',
+  'refere': url,
+  'cookie': """your cookie value ( you can get that from your web page) """
+}
   try:
-      r = requests.get(url)
-      if(r.status_code != 200):
-          raise Exception("Error: Status code is not 200")
+      req = Request(url, headers=headers)
+      # get the status code of the req
+      res = urlopen(req)
+      print(res.status)
+      if(res.status == 200):
+          return res.read()   
       else:
-          print("Success: Status code is 200")
-          return r.content
+          raise Exception("Error: Status code is not 200")
   except:
       return "Error: Status code is not 200"
   
@@ -19,12 +31,15 @@ def saveHTML(htmlContent, fileName):
   with open(fileName+'.html', 'w', encoding='utf-8') as f:
     f.write(f'''{htmlContent}''')
     f.close()
-
-websiteSoup = BeautifulSoup(scrapeWebsite('https://www.geeksforgeeks.org/python-programming-language/'), "html.parser")
-saveHTML(websiteSoup.prettify(), websiteSoup.title.string)
-aTags = websiteSoup.find_all('a', href=True)
-amountOfLinks = len(aTags)
-print(f"There are {amountOfLinks} links on this page")
+#
+websiteSoup =  BeautifulSoup(scrapeWebsite('https://www.classcentral.com'), "html.parser")
+if websiteSoup.title.string:
+  saveHTML(websiteSoup.prettify(), websiteSoup.title.string)
+else:
+  saveHTML(websiteSoup.prettify(), "NoTitle")
+# aTags = websiteSoup.find_all('a', href=True)
+# amountOfLinks = len(aTags)
+# print(f"There are {amountOfLinks} links on this page")
 # for tag in aTags:
 #     print(tag.get('href'))
 
