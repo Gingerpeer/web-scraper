387575ce1aca6cce65943563383ce9f3086c70ca
Gingerpeer PC <pierrevanzyl777@gmail.com>
2023-02-21 12:20:48 +0200

    initial commit

---
+++ webScraper.py
@@ -0,0 +1,34 @@
+# write a web scraper that will scrape the following website: 
+# This is a web scraping python project where the html is collected from a website. The Html is then used to create an Html document. All links in the main website provided are also scraped and html files created with their page titles as the names
+import requests
+from bs4 import BeautifulSoup
+# Making a GET request
+def scrapeWebsite(url):
+  try:
+      r = requests.get(url)
+      if(r.status_code != 200):
+          raise Exception("Error: Status code is not 200")
+      else:
+          print("Success: Status code is 200")
+          return r.content
+  except:
+      return "Error: Status code is not 200"
+  
+# function to save html content to a html file
+def saveHTML(htmlContent, fileName):
+  with open(fileName+'.html', 'w', encoding='utf-8') as f:
+    f.write(f'''{htmlContent}''')
+    f.close()
+
+websiteSoup = BeautifulSoup(scrapeWebsite('https://www.geeksforgeeks.org/python-programming-language/'), "html.parser")
+saveHTML(websiteSoup.prettify(), websiteSoup.title.string)
+aTags = websiteSoup.find_all('a')
+for tag in aTags:
+    print(tag.get('href'))
+
+# # check status code for response received
+# # success code - 200
+# print(type(r))
+ 
+# print content of request
+# print(r.content)
